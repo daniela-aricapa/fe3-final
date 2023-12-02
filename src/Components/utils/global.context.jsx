0b47362c6ffabc96b140a3cc9e8d5a46ba7aa79d
daniela-aricapa <dani6545@hotmail.com>
2023-12-02 07:57:44 -0500

    Actualización II

--- src/Components/utils/global.context.jsx
+++ src/Components/utils/global.context.jsx
@@ -1,18 +1,17 @@
-// global.context.js (Contexto global)
+
 import React, { createContext, useMemo, useState } from "react";
 
-// Define el estado inicial del contexto
+
 const initialState = { theme: "light", data: [] };
 
-// Crea el contexto
+
 export const GlobalContext = createContext(initialState);
 
-// Proveedor del contexto
+
 export const GlobalContextProvider = ({ children }) => {
-  // Utiliza useState para gestionar el estado
+  
   const [globalState, setGlobalState] = useState(initialState);
 
-  // Implementa lógica personalizada si es necesario, por ejemplo, funciones para actualizar el estado
 
   const updateTheme = (newTheme) => {
     setGlobalState((prevState) => ({
@@ -21,11 +20,11 @@ export const GlobalContextProvider = ({ children }) => {
     }));
   };
 
-  // Define los valores que deseas proporcionar en el contexto aquí
+  
   const contextValue = useMemo(() => {
     return {
       state: globalState,
-      updateTheme, // Agrega aquí otras funciones o estados según tus necesidades
+      updateTheme, 
     };
   }, [globalState]);
 
